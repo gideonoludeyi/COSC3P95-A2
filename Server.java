8dd85c5c90bc602b1408d237161a697c9363828f
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2023-11-10 02:14:30 -0500

    fork/join

--- Server.java
+++ Server.java
@@ -3,8 +3,10 @@ import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.*;
 import java.nio.*;
+import java.util.concurrent.*;
 
 public class Server implements Runnable {
+  private final ForkJoinPool pool = ForkJoinPool.commonPool();
   private final int port;
   private final Path outdir;
 
@@ -18,26 +20,31 @@ public class Server implements Runnable {
     System.out.println("Listening...");
     try (ServerSocket socket = new ServerSocket(port)) {
       while (!socket.isClosed()) {
-        try (Socket client = socket.accept()) {
-          InputStream in = client.getInputStream();
-
-          int namesize = ByteBuffer.wrap(in.readNBytes(Integer.BYTES))
-              .getInt();
-          String filename = new String(in.readNBytes(namesize), StandardCharsets.UTF_8);
-
-          String filepath = outdir.resolve(filename).toString();
-          try (FileOutputStream out = new FileOutputStream(filepath)) {
-            long contentsize = in.transferTo(out);
-            System.out.printf("Wrote %s bytes to %s%n", contentsize, filepath);
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+        Socket client = socket.accept();
+        pool.execute(() -> process(client));
       }
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
       System.out.println("Closed");
+    }
+  }
+
+  private void process(Socket client) {
+    try (client) {
+      InputStream in = client.getInputStream();
+
+      int namesize = ByteBuffer.wrap(in.readNBytes(Integer.BYTES))
+          .getInt();
+      String filename = new String(in.readNBytes(namesize), StandardCharsets.UTF_8);
+
+      String filepath = outdir.resolve(filename).toString();
+      try (FileOutputStream out = new FileOutputStream(filepath)) {
+        long contentsize = in.transferTo(out);
+        System.out.printf("Wrote %s bytes to %s%n", contentsize, filepath);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
